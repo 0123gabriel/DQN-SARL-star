d4178e14b1f0a81177e46161581cfa498d8d5e3a
Gabriel Garcia <jgabrielgarciaa@gmail.com>
2025-05-30 19:24:01 +0900

    gif title changed

--- crowd_nav/test.py
+++ crowd_nav/test.py
@@ -123,7 +123,7 @@ def main():
             last_pos = current_pos
         
         logging.info('It takes %.2f seconds to finish. Final status is %s', env.global_time, info)
-        env.render('video', args.video_file, args.model_dir, args.test_case)
+        env.render('video', args.video_file, args.model_dir.split('/')[1], args.test_case)
 
         print(args.model_dir)
 
